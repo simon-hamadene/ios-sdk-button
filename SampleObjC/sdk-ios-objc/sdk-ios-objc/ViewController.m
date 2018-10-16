67f9e973afbcdb509c2cf9cd301807c43ca3208a
Simon Hamadene <simon.hamadene@yoti.com>
2018-10-16 14:27:48 +0100

    E-907: Created the ViewController’s UI. Created the ProfileViewController class

---
+++ SampleObjC/sdk-ios-objc/sdk-ios-objc/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  sdk-ios-objc
+//
+//  Created by Simon Hamadene on 16/10/2018.
+//  Copyright © 2018 Yoti Limited. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
